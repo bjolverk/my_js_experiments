4081e70032b34e0ac77fe5c44d2eeaf8cebc15f9
Anton <Bjolverk@gmail.com>
2018-11-12 16:54:49 +0300

    Продолжаю наводить порядок

--- Block2/cycles/javaScript.js
+++ Block2/cycles/javaScript.js
@@ -1,34 +1,21 @@
 let num = 50;
-/*
-if (num < 49) {
-    console.log("Не верно!");
-}else if(num > 100){
-    console.log("Много!");
-}else{
-    console.log("Верно!");
-}
 
-(num == 50) ? console.log("Верно!") : console.log("Неверно!"); // Тернарный оператор
-
-switch (num) {
-    case num < 49:
-        console.log("Не верно!");
-        break;
-    case num > 100:
-        console.log("Много!");
-        break;
-    case num > 80:
-        console.log("всё еще много");
-        break;
-    case  50: //Записывать надо ИМЕННО ТАК
-        console.log("Верно!");
-        break;
-    default:
-        console.log("Что - то пошло не так");
-        break;
-}
-*/
 while (num < 55) {
     console.log(num);
     num++;
+}
+console.log("-------------------------");
+
+do {
+    console.log(num);
+    console.log("-------------------------");
+    num++;
+}while(num < 55);
+
+
+for(let i = 1; i <= 100; i++){
+    if(i % 3 == 0){
+        continue;
+    }
+    console.log(i);
 }
