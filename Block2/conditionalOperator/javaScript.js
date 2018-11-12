3f9742eae59ef0f5f99d04f9bcc6d0727c735f0e
Anton <Bjolverk@gmail.com>
2018-11-12 16:40:55 +0300

    Упорядочеваем второй блок

---
+++ Block2/conditionalOperator/javaScript.js
@@ -0,0 +1,34 @@
+let num = 50;
+/*
+if (num < 49) {
+    console.log("Не верно!");
+}else if(num > 100){
+    console.log("Много!");
+}else{
+    console.log("Верно!");
+}
+
+(num == 50) ? console.log("Верно!") : console.log("Неверно!"); // Тернарный оператор
+
+switch (num) {
+    case num < 49:
+        console.log("Не верно!");
+        break;
+    case num > 100:
+        console.log("Много!");
+        break;
+    case num > 80:
+        console.log("всё еще много");
+        break;
+    case  50: //Записывать надо ИМЕННО ТАК
+        console.log("Верно!");
+        break;
+    default:
+        console.log("Что - то пошло не так");
+        break;
+}
+*/
+while (num < 55) {
+    console.log(num);
+    num++;
+}
