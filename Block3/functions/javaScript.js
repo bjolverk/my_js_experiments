9557d0291ada27c23779482cfab25d022b943ad2
Anton <Bjolverk@gmail.com>
2018-11-19 18:11:15 +0300

    19.11.2018

--- Block3/functions/javaScript.js
+++ Block3/functions/javaScript.js
@@ -45,3 +45,45 @@ let twelve = "12.2px";
 //console.log(Math.round(twelve));
 console.log(parseInt(twelve));
 console.log(parseFloat(twelve));
+
+/*
+var counter = (function () {
+    var current = 0;
+    return function () {
+      current++;
+      return current;
+    }
+  })();
+  
+  console.log(counter()); // 1
+  console.log(counter()); // 2
+  console.log(counter()); // 3
+  console.log(counter()); // 4
+*/
+/*
+function createCounter(){
+    let counter = 0;
+    let myFunction = function(){
+        return ++counter;
+    }
+    return myFunction;
+}
+let increment = createCounter();
+
+for(let i=0; i<10; i++){
+    let c = increment();
+    console.log(c);
+}
+*/
+
+function counter() {
+    let i = 2;
+    return function(){
+        return i=Math.pow(i, 5);
+    }
+}
+let a = counter();
+console.log(a());
+console.log(a());
+console.log(a());
+console.log(a());
---
+++ Block4/functions/javaScript.js
@@ -0,0 +1,20 @@
+function first() {
+    setTimeout( function(){
+        console.log(1);
+    }, 10 );
+}
+function second(){
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback){
+    console.log("Я учу " + lang);
+    callback();
+}
+
+learnJS("JavaScript", function(){
+    console.log("Я прошёл третий урок");
+});
