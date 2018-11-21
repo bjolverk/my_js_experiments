3ffb487ebe7b68e9001176746a958bbd35a69fe4
Anton <Bjolverk@gmail.com>
2018-11-21 14:14:02 +0300

    Update javaScript.js

--- Block5/Objects/javaScript.js
+++ Block5/Objects/javaScript.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let options = {
     width : 1024,
     height : 1024,
@@ -13,8 +13,8 @@ options.colors={
 
 };
 
-=======
 // let obj = new Object();
+
 options = {
     width: 1024,
     height:1024,
@@ -29,19 +29,18 @@ options.colors = {
     border:"black",
     bg:"red"
 };
->>>>>>> 9f5cb88bb347cb1b2e2d4650070a8c06097ac3c2
+
 delete options.bool;
 
 console.log(options);
 
-<<<<<<< HEAD
 for(let key in options){
     console.log('Свойство ' + key + ' имеет значение ' + options[key]);
 };
-=======
+
 for (let key in options){
     console.log('Свойство ' +  key + ' имеет значение ' + options[key]);
 };
 
 console.log(Object.keys(options).length);
->>>>>>> 9f5cb88bb347cb1b2e2d4650070a8c06097ac3c2
+
