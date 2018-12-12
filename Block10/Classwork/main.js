77c11cd82fcd711d3bdcc251f33ccbf3919b9f81
Anton <Bjolverk@gmail.com>
2018-12-12 18:20:23 +0300

    My First tabs 12.12.2018

---
+++ Block10/Classwork/main.js
@@ -0,0 +1,18 @@
+//setTimeout(sayHello, 3000); // Время в миллисекундах
+//let timerId= setTimeout(sayHello, 3000); // Это- таймер для однократного срабатывания
+//let timerId = setInterval(sayHello, 3000); // Это для многократного повторения
+//clearTimeout(timerId);
+//setInterval() Когда таймер с интервалом работает, он не учитывает, как долго функция отрабатывает
+// т.е он не отслеживает, отработала ли функция или нет
+// setTimeout отслеживает
+/*
+function sayHello() {
+    alert('Hello, World!');
+}
+*/
+/*
+let timerId = setTimeout(function log(){
+    console.log("Hello");
+    setTimeout(log, 2000); // Рекурсия!
+});
+*/
